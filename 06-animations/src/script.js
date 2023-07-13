e3002f350be86121e0a3a7065f0b33b1cfa79442
NguyenCatNguyen <catngn11@gmail.com>
2023-07-13 15:41:17 -0500

    Update script.js

--- 06-animations/src/script.js
+++ 06-animations/src/script.js
@@ -19,7 +19,7 @@ const sizes = {
 }
 
 // Camera
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height) // Field of view, aspect ratio
 camera.position.z = 3
 scene.add(camera)
 
